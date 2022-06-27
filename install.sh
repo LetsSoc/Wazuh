946181ae89b3357fc2a21893eba509bfbfd0ab21
LetsSoc <107577062+LetsSoc@users.noreply.github.com>
2022-06-27 15:16:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+apt install gnupg -y
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
 echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 apt-get update
