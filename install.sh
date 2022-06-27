6e34f9972e26731159d7619a3c5ef770b3127547
LetsSoc <107577062+LetsSoc@users.noreply.github.com>
2022-06-27 15:12:35 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt-get update
+WAZUH_MANAGER="10.0.102.2" apt-get install wazuh-agent
+systemctl daemon-reload
+systemctl enable wazuh-agent
+systemctl start wazuh-agent
+sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
+apt-get update
+echo "wazuh-agent hold" | dpkg --set-selections
